0d866018baf2c655fe6def179f68823e369bcbca
Ash <15804805+abloom12@users.noreply.github.com>
2026-04-29 22:44:50 -0400

    made sure req headers in trpc match better auth

--- apps/api/src/trpc/context.ts
+++ apps/api/src/trpc/context.ts
@@ -1,13 +1,14 @@
 import { type CreateFastifyContextOptions } from '@trpc/server/adapters/fastify';
+import { fromNodeHeaders } from 'better-auth/node';
 
 import { auth } from '../lib/auth';
 
 export async function createContext({ req, res }: CreateFastifyContextOptions) {
-  const session = await auth.api.getSession({ headers: req.headers });
+  const session = await auth.api.getSession({
+    headers: fromNodeHeaders(req.headers),
+  });
+
   return { req, res, session };
 }
 
 export type Context = Awaited<ReturnType<typeof createContext>>;
-
-// @ianvs/prettier-plugin-sort-imports
-// prettier-plugin-tailwindcss
